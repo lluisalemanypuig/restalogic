128cb46b9b05467e01b3fda45d9147842b0096a6
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2023-06-22 16:15:38 +0200

    Implement correct decrement of all elements in 'graella'

--- restalogic.js
+++ restalogic.js
@@ -42,7 +42,8 @@ function llista_paraules_trobades() {
 		
 		if (i < children.length) {
 			
-			console.log(i, children[i].textContent, children[i].textContent.length);
+			//console.log(i, children[i].textContent, children[i].textContent.length);
+			
 			all_words.push( children[i].textContent );
 		}
 	}
@@ -50,44 +51,11 @@ function llista_paraules_trobades() {
 	return all_words;
 }
 
-/*
-// posar la graella a tot zeros (codi de practica)
-function graella_a_zeros() {
-	// 'graella' object
-	graella_body = 
-		document.getElementById("table_graella")
-		.getElementsByTagName("tbody");
-	
-	if (graella_body.length == 0) {
-		console.log("graella is not loaded")
-		return;
-	}
-	else {
-		graella_body = graella_body[0];
-		header = graella_body.getElementsByTagName("tr")[0];
-		
-		for (var i = 1; i < graella_body.rows.length; ++i) {
-			row = graella_body.getElementsByTagName("tr")[i];
-			console.log(row, row.childElementCount);
-			
-			for (var j = 1; j < row.childElementCount; ++j) {
-				cell = row.getElementsByTagName("th")[j];
-				console.log(i, j, cell.childNodes[0]);
-				
-				if (cell.childNodes[0] != null) {
-					cell.childNodes[0].textContent = "0";
-				}
-			}
-		}
-	}
-}
-*/
-
 function troba_lletres() {
 	var lletres = [];
 	
 	var hex_grid = document.getElementById("hex-grid");
-	console.log(hex_grid);
+	//console.log(hex_grid);
 	
 	for (var i = 0; i < 7; ++i) {
 		var lletra = hex_grid.childNodes[i].childNodes[0].childNodes[0].textContent;
@@ -107,50 +75,149 @@ function troba_lletres() {
 }
 
 // actualitza cel·la: resta en 1 el valor actual de la cel·la
-function actualitza_cella(cell) {
-	var count = parseInt(cell.childNodes[0].textContent, 10);
-	console.log("count:", count);
-	--count;
-	cell.childNodes[0].textContent = count;
+function actualitza_cella(copy_contents_graella, i, j, cell) {
+	//console.log("    i=", i, "j=", j);
+	
+	//console.log("    count=", copy_contents_graella[i][j]);
+	--copy_contents_graella[i][j];
+	//console.log("    count=", copy_contents_graella[i][j]);
+	
+	cell.childNodes[0].textContent = copy_contents_graella[i][j];
 }
 
-function actualitza_graella(event) {
-	// llistat de paraules
-	var llista_paraules = llista_paraules_trobades();
-	console.log("Paraules trobades:", llista_paraules);
-	
-	// lletres del paraulogic del dia
-	var map = troba_lletres();
-	console.log("Lletres del dia:", map);
-	
+var first_access = true;
+var contents_graella = null;
+var num_rows;
+var num_cols;
+var map_letters;
+
+function get_contents_graella() {
 	// 'graella' object
-	graella_body = 
+	var graella_body = 
 		document.getElementById("table_graella")
 		.getElementsByTagName("tbody");
 	
 	if (graella_body.length == 0) {
-		console.log("graella is not loaded")
+		//console.log("graella is not loaded")
 		return;
 	}
-	else {
-		graella_body = graella_body[0];
-		header = graella_body.getElementsByTagName("tr")[0];
+	
+	contents_graella = new Array(num_rows);
+	//console.log("contents_graella.length=", contents_graella.length);
+	
+	graella_body = graella_body[0];
+	var header = graella_body.getElementsByTagName("tr")[0];
+	
+	//console.log("graella_body.rows.length=", graella_body.rows.length);
+	for (var i = 1; i < graella_body.rows.length; ++i) {
+		contents_graella[i - 1] = [];
 		
-		for (var i = 0; i < llista_paraules.length; ++i) {
-			var paraula = llista_paraules[i];
-			console.log("actualitzant per la paraula:", paraula);
+		var row = graella_body.getElementsByTagName("tr")[i];
+		//console.log("i=", i, row);
+		
+		for (var j = 1; j < row.childElementCount; ++j) {
+			var cell = row.getElementsByTagName("th")[j];
 			
-			var inicial = paraula[0];
-			
-			var index_inicial = map.get(inicial);
-			var index_longitud = (paraula.length - 3) + 1;
-			
-			row = graella_body.getElementsByTagName("tr")[ index_inicial ];
-			cell = row.getElementsByTagName("th")[ index_longitud ];
-			
-			actualitza_cella(cell);
+			if (cell.childNodes[0] != null) {
+				var data = parseInt(cell.childNodes[0].textContent, 10);
+				//console.log( i - 1, data );
+				
+				contents_graella[i - 1].push( data );
+			}
 		}
 	}
+	
+	//console.log(contents_graella);
+}
+
+function actualitza_graella(event) {
+	if (first_access) {
+		// lletres del paraulogic del dia
+		map_letters = troba_lletres();
+		//console.log("Lletres del dia:", map_letters);
+		
+		num_rows = map_letters.size + 1;
+		//console.log("num_rows=", num_rows);
+		
+		//console.log("First access");
+		contents_graella = [];
+		for (var i = 0; i < 7; ++i) {
+			contents_graella[i] = Array(7);
+			for (var j = 0; j < 7; ++j) {
+				contents_graella[i][j] = 0;
+			}
+		}
+		get_contents_graella();
+		
+		num_rows = contents_graella.length;
+		num_cols = contents_graella[0].length;
+		
+		//console.log("num_cols=", num_cols);
+		
+		first_access = false;
+	}
+	
+	// deep copy 'contents_graella'
+	var copy = Array(7);
+	for (var i = 0; i < 7; ++i) {
+		copy[i] = [...contents_graella[i]];
+	}
+	
+	// llistat de paraules
+	var llista_paraules = llista_paraules_trobades();
+	//console.log("Paraules trobades:", llista_paraules);
+	
+	// 'graella' object
+	var graella_body = 
+		document.getElementById("table_graella")
+		.getElementsByTagName("tbody");
+	
+	if (graella_body.length == 0) {
+		//console.log("graella is not loaded")
+		return;
+	}
+	
+	graella_body = graella_body[0];
+	var header = graella_body.getElementsByTagName("tr")[0];
+	var bottom = graella_body.getElementsByTagName("tr")[num_rows];
+	
+	//console.log("header:", header);
+	//console.log("bottom:", bottom);
+	
+	for (var i = 0; i < llista_paraules.length; ++i) {
+		var paraula = llista_paraules[i];
+		//console.log("actualitzant per la paraula:", paraula);
+		
+		var inicial = paraula[0];
+		
+		var index_inicial = map_letters.get(inicial);
+		var index_longitud = (paraula.length - 3) + 1;
+		
+		var row = graella_body.getElementsByTagName("tr")[ index_inicial ];
+		//console.log("    ", row);
+		
+		var cell = row.getElementsByTagName("th")[ index_longitud ];
+		
+		var ii = index_inicial - 1;
+		var jj = index_longitud - 1;
+		
+		//console.log("    Update individual cell");
+		//console.log("    i=", ii, " j=", jj);
+		actualitza_cella(copy, ii, jj, cell);
+		
+		//console.log("    count=", copy[ii][jj]);
+		
+		//console.log("Update total letter count in row", ii);
+		actualitza_cella(copy, ii, num_cols - 1, row.lastChild);
+		
+		//console.log("Update total length count in column", jj);
+		actualitza_cella(copy, num_rows - 1, jj, bottom.getElementsByTagName("th")[ index_longitud ]);
+		
+		//console.log("Update total count in column", jj);
+		actualitza_cella(copy, num_rows - 1, num_cols - 1, bottom.children[num_cols]);
+	}
+	
+	//console.log("copy_contents_graella:", copy);
 }
 
 document.getElementById("pistes-link").addEventListener('click', actualitza_graella);
