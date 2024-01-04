ba20c07c068402b41862f650c0a3ff5970c1f886
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2024-01-04 16:20:57 +0100

    Better check for new version pending

--- new_version_pending.sh
+++ new_version_pending.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$(git log -1 --pretty=%B)" == "New version" ]; then
+if [[ "$(git log -1 --pretty=%B)" == *"New version"* ]]; then
 	echo "No"
 else
 	echo "Yes, a new version is pending"
