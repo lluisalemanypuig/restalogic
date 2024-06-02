ce9a9eb6801cc8515db587a57f075755ab039df4
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2024-06-02 19:03:02 +0200

    Reestructured project

--- compress_firefox.sh
+++ compress_firefox.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-zip -r -FS ../restalogic.zip * \
-	--exclude '*.git' \
-	--exclude 'compress_firefox.sh' \
-	--exclude '*.html' \
-	--exclude 'README.md' \
-	--exclude 'new_version_pending.sh'
+cd src/
+zip -r -FS ../restalogic.zip *
+cd ..
