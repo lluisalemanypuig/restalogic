6de3437cb84ea8ed90357a00ea8089a65c23bd10
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2024-06-27 15:39:06 +0200

    Better message errors

--- find_multitoken_words.py
+++ find_multitoken_words.py
@@ -1,57 +1,86 @@
-def clean_string(s):
+NO_MULTITOKEN_WORDS=0
+EXIST_MULTITOKEN_WORDS=1
+UNKNOWN_MULTITOKEN_WORDS=2
+SOLUTION_MAP_NOT_FOUND=3
+INDEX_FILE_COULD_NOT_BE_OPENED=4
+
+def exit_with_code(code):
+	print(f"Exit with code: {code}")
+	if code == NO_MULTITOKEN_WORDS:
+		print("Python did not find multitoken words")
+	elif code == EXIST_MULTITOKEN_WORDS:
+		print("Python found multitoken words but these are already in the addon.")
+	elif code == UNKNOWN_MULTITOKEN_WORDS:
+		print("Python found multitoken words that are not in the addon yet.")
+	elif code == SOLUTION_MAP_NOT_FOUND:
+		print("Error: Python could not find the map that contains today's solution.")
+	elif code == INDEX_FILE_COULD_NOT_BE_OPENED:
+		print("Error: Python could not open the index.html file.")
+	
+	exit(code)
+
+def trim_string(s):
 	if s[0] == '\t': s = s[1:]
 	if s[-1] == '\n': s = s[:-1]
 	return s
 
 def retrieve_known_multitoken_words():
+	"""
+	This function is assumed to always succeed. The script 'src/restalogic.js' is
+	assumed to be always readable.
+	"""
 	known_multitoken_words = None
 	with open('src/restalogic.js', 'r') as f:
-		known_multitoken_words = ''.join(list(map(lambda s: clean_string(s), f.readlines()[126:139])))
+		known_multitoken_words = ''.join(list(map(lambda s: trim_string(s), f.readlines()[126:139])))
 		known_multitoken_words = "{" + known_multitoken_words + "}"
 		known_multitoken_words = list(eval(known_multitoken_words).keys())
+	
+	# a little assert to ensure that the code above succeeded
+	assert(known_multitoken_words is not None)
 	return known_multitoken_words
 
 known_multitoken_words = retrieve_known_multitoken_words()
 
-NOTHING_TO_DO=0
-EXIST_MULTITOKEN_WORDS=1
-UNKNOWN_MULTITOKEN_WORDS=2
-
+entire_file = ""
 with open("index.html", 'r') as f:
-	
-	entire_file = ""
 	for line in f: entire_file += line[:-1]
+
+if entire_file == "":
+	exit_with_code(INDEX_FILE_COULD_NOT_BE_OPENED)
+
+ini = entire_file.find("var t={")
+if ini == -1:
+	exit_with_code(SOLUTION_MAP_NOT_FOUND)
+
+fin = entire_file.find("};", ini)
+
+solution = eval(entire_file[ini+6:fin+1])
+
+multitoken_words = False
+unknown_multitoken_words = False
+for key, value in solution["p"].items():
 	
-	ini = entire_file.find("var t={")
-	fin = entire_file.find("};", ini)
+	if value.find(" o ") != -1:
+		value = value.split(" o ")
 	
-	solution = eval(entire_file[ini+6:fin+1])
+	if type(value) == str:
+		value = [value]
 	
-	multitoken_words = False
-	unknown_multitoken_words = False
-	for key, value in solution["p"].items():
+	for w in filter(lambda s: s.find(" ") != -1, value):
+		print(f"Inspecting '{w}'")
 		
-		if value.find(" o ") != -1:
-			value = value.split(" o ")
+		multitoken_words = True
 		
-		if type(value) == str:
-			value = [value]
+		is_known = any(map(lambda known: known == w, known_multitoken_words))
+		print(f"{key}: {value} -- {is_known}")
 		
-		for w in filter(lambda s: s.find(" ") != -1, value):
-			print(f"Inspecting '{w}'")
-			
-			multitoken_words = True
-			
-			is_known = any(map(lambda known: known == w, known_multitoken_words))
-			print(f"{key}: {value} -- {is_known}")
-			
-			if not is_known:
-				unknown_multitoken_words = True
-	
-	if unknown_multitoken_words:
-		exit(UNKNOWN_MULTITOKEN_WORDS)
-	
-	if multitoken_words:
-		exit(EXIST_MULTITOKEN_WORDS)
-	
-	exit(NOTHING_TO_DO)
+		if not is_known:
+			unknown_multitoken_words = True
+
+if unknown_multitoken_words:
+	exit_with_code(UNKNOWN_MULTITOKEN_WORDS)
+
+if multitoken_words:
+	exit_with_code(EXIST_MULTITOKEN_WORDS)
+
+exit_with_code(NO_MULTITOKEN_WORDS)
--- find_multitoken_words.sh
+++ find_multitoken_words.sh
@@ -5,6 +5,8 @@ wget https://www.vilaweb.cat/paraulogic -O index.html
 python3 find_multitoken_words.py
 res=$?
 
-if [ $res -eq 2 ]; then
+if [ $res -ge 2 ]; then
+	echo "The job failed with error code $res"
 	exit 1
 fi
+echo "The job succeeded"
